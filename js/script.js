b63a2d706a97e4cb6fc22e4e31a649be23af5ab4
ilhamgayyy <ilhamf0123@gmail.com>
2024-08-04 16:54:31 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+  // ham
+  const ham = document.querySelector('#ham'); 
+  const navMenu = document.querySelector('#nav-menu');
+  
+  ham.addEventListener('click', function(){
+   ham.classList.toggle('ham-active');
+   navMenu.classList.toggle('hidden');
+  });
+      //navbar fixed
+  window.onscroll = function(){
+      const header = document.querySelector('header');
+      const fixedNav = header.offsetTop;
+      
+      if(window.pageYOffset > fixedNav){
+          header.classList.add('navbar-fixed');
+      }else{
+          header.classList.remove('navbar-fixed');
+      }
+  }
